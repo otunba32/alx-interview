88678eb85b0197f0fd44242fa1bbe92ed2a98035
Emmanuel Abia <emmanuel.abia32@gmail.com>
2024-01-04 23:42:57 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,19 +4,30 @@
 Pascal's Triangle
 """
 
-def pascal_triangle(n):
-    triangle = []
-    for i in range(n):
-        row = []
-        for x in range(i + 1):
-            if x == 0 or x == i:
-                row.append(1)
-            else:
-                row.append(triangle[i - 1][x] + triangle[i - 1][x - 1])
-        triangle.append(row)
-    return triangle
 
-"""# Call the function with n = 5
-result = pascal_triangle(5)
-print(result)"""
+def pascal_triangle(n):
+    """Generate Pascal's Triangle up to the specified row `n`.
+
+    Args:
+        n (int): Number of rows in Pascal's Triangle.
+
+    Returns:
+        list: List of lists representing Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        curr_row = [1]
+
+        for j in range(1, i):
+            curr_row.append(prev_row[j - 1] + prev_row[j])
+
+        curr_row.append(1)
+        triangle.append(curr_row)
+
+    return triangle
 
